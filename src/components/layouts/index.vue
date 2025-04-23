5fee145a1a09b07dcdf05a99283ab4797148d389
kentxxq <kentxxq@qq.com>
2025-04-23 14:20:25 +0800

    导航栏完成

--- src/components/layouts/index.vue
+++ src/components/layouts/index.vue
@@ -1,13 +1,17 @@
 <template>
   <!-- 全屏 -->
   <div class="h-screen w-screen flex flex-col">
-    <Menubar class="px-14 h-14 border-0" :model="items">
+    <Menubar class="px-14 h-14 border-0 border-b" :model="items">
       <template #start>
-        <i-emojione-monotone-letter-n class=" text-4xl" style="color:var(--p-primary-color)" />
+        <div class="flex items-center justify-center gap-2">
+          <i-emojione-monotone-letter-n class=" text-4xl" style="color:var(--p-primary-color)" />
+          <span class="text-4xl font-bold">NAV</span>
+        </div>
       </template>
       <template #end>
         <div class="flex items-center gap-4">
-          <a href="xxx"><i-simple-icons-telegram class="text-2xl" style=" color:var(--p-primary-color);" /></a>
+          <a href="https://t.me/kentxxqdev/79" target="_blank"><i-simple-icons-telegram class="text-2xl"
+              style=" color:var(--p-primary-color);" /></a>
           <Button label="登录" size="small" />
         </div>
       </template>
