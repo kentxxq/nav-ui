ba6b0144dc8b73a6ac6cfa40b2306b90f774058c
kentxxq <kentxxq@qq.com>
2025-04-28 18:22:02 +0800

    完成用例

--- src/pages/home/add-bookmark.vue
+++ src/pages/home/add-bookmark.vue
@@ -1,28 +1,28 @@
 <template>
   <Dialog v-model:visible="visible" modal header="新增书签" class="w-100" :dismissableMask="true">
-    <Form v-slot="$form" :initialValues :resolver="resolver" @submit="onFormSubmit" :validateOnValueUpdate="true"
-      class="flex justify-center flex-col gap-4">
+    <Form v-slot="$form" :initial-values="createBookmarkRO" :resolver="resolver" @submit="onFormSubmit"
+      :validateOnValueUpdate="true" class="flex justify-center flex-col gap-4">
       <div class="flex flex-col gap-1">
-        <InputText :inputProps="{ autocomplete: 'true' }" name="url" placeholder="站点链接" toggleMask :feedback="false"
-          fluid />
+        <InputText v-model="createBookmarkRO.url" :inputProps="{ autocomplete: 'true' }" name="url" placeholder="站点链接"
+          toggleMask :feedback="false" fluid />
         <Message v-if="$form.url?.invalid" severity="error" size="small" variant="simple">
           {{ $form.url.error?.message }}</Message>
       </div>
       <div class="flex flex-col gap-1">
-        <InputText :inputProps="{ autocomplete: 'true' }" name="title" placeholder="标题" toggleMask :feedback="false"
-          fluid />
+        <InputText v-model="createBookmarkRO.title" :inputProps="{ autocomplete: 'true' }" name="title" placeholder="标题"
+          toggleMask :feedback="false" fluid />
         <Message v-if="$form.title?.invalid" severity="error" size="small" variant="simple">
           {{ $form.title.error?.message }}</Message>
       </div>
       <div class="flex flex-col gap-1">
-        <InputText :inputProps="{ autocomplete: 'true' }" name="iconUrl" placeholder="图标链接" toggleMask :feedback="false"
-          fluid />
+        <InputText v-model="createBookmarkRO.iconUrl" :inputProps="{ autocomplete: 'true' }" name="iconUrl"
+          placeholder="图标链接" toggleMask :feedback="false" fluid />
         <Message v-if="$form.iconUrl?.invalid" severity="error" size="small" variant="simple">
           {{ $form.iconUrl.error?.message }}</Message>
       </div>
       <div class="flex flex-col gap-1">
-        <InputText :inputProps="{ autocomplete: 'true' }" name="description" placeholder="说明" toggleMask
-          :feedback="false" fluid />
+        <InputText v-model="createBookmarkRO.description" :inputProps="{ autocomplete: 'true' }" name="description"
+          placeholder="说明" toggleMask :feedback="false" fluid />
         <Message v-if="$form.description?.invalid" severity="error" size="small" variant="simple">
           {{ $form.description.error?.message }}</Message>
       </div>
@@ -49,11 +49,11 @@ defineOptions({
 })
 
 const visible = defineModel<boolean>('addBookmarkVisible', { required: true })
-const initialValues = reactive<CreateBookmarkRO>({
+const createBookmarkRO = reactive<CreateBookmarkRO>({
   url: '',
   title: '',
   iconUrl: '',
-  description: null,
+  description: null
 });
 const resolver = ref(zodResolver(z.object({
   url: z.string(),
@@ -63,7 +63,7 @@ const resolver = ref(zodResolver(z.object({
 })));
 const onFormSubmit = async (e: FormSubmitEvent) => {
   if (e.valid) {
-    const bookmark = await bookmarkCreateBookmark({ url: e.values.url, title: e.values.title, iconUrl: e.values.iconUrl, description: e.values.description })
+    const bookmark = await bookmarkCreateBookmark(createBookmarkRO)
     myToast.success('新增书签成功')
     visible.value = false
     console.log(bookmark)
